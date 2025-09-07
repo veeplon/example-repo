679ec9e55c21e93f2d007167cbca2c7e795b0e5f
veeplon <veeplonmail@gmail.com>
2025-09-07 15:13:50 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,5 +2,8 @@ import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 
 console.log("Hello world!")
 EventsSDK.on("GameStarted", () => {
-	console.log("Hello world!")
+	console.log("Game started!")
+})
+EventsSDK.on("GameEnded", () => {
+	console.log("Game ended!")
 })
